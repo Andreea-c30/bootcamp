7041c5b97ec156a2a104b58e89f2e913a37474f0
Andreea Chiper <c.1andreea874@gmail.com>
2021-06-16 11:29:37 +0300

    Added the php file

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<p>Nume: Chiper </p>
+<p>Prenume: Andreea</p>
+<p>Tel: 024342342</p>
+<br>
+<div>
+        <?php echo "Today is " . date("Y/m/d H:i:s"); ?>
+    </div>
+</body>
+</html>
